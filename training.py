c61526b1c7c17218b323c35e8a3f2ee13960629c
yubing-hi <yubingvoo@163.com>
2026-03-13 21:48:01 +0800

    修改了打印loss的逻辑，打印当前整个batch的loss

--- training.py
+++ training.py
@@ -14,12 +14,14 @@ def train(model, train_dataloader, epochs, lr, loss_fn, val_dataloader=None, cli
 
     train_losses = []
     for epoch in range(epochs):
+        epoch_loss = 0
+        batch_total = 0
 
+        start_time = time.time()
         for step, (model_input, gt) in enumerate(train_dataloader):
             model_input = {k: v.to(device) for k, v in model_input.items()}
             gt = {k: v.to(device) for k, v in gt.items()}
-            start_time = time.time()
-        
+
             model_output = model(model_input)
             losses = loss_fn(model_output, gt)
 
@@ -28,6 +30,8 @@ def train(model, train_dataloader, epochs, lr, loss_fn, val_dataloader=None, cli
                 single_loss = loss.mean()
                 train_loss += single_loss
 
+            epoch_loss += train_loss.item()
+            batch_total += 1
             train_losses.append(train_loss.item())
 
             optim.zero_grad()
@@ -41,21 +45,29 @@ def train(model, train_dataloader, epochs, lr, loss_fn, val_dataloader=None, cli
 
             optim.step()
 
-        #print("Running validation set...")
+        avg_train_loss = epoch_loss / max(batch_total, 1)
+
         model.eval()
         with torch.no_grad():
-            val_loss = 0.
+            val_loss_sum = 0.0
+            val_batch_count = 0
             for (model_input, gt) in val_dataloader:
                 model_input = {k: v.to(device) for k, v in model_input.items()}
                 gt = {k: v.to(device) for k, v in gt.items()}
                 model_output = model(model_input)
                 losses = loss_fn(model_output, gt)
-            for loss_name, loss in losses.items():
-                single_loss = loss.mean()
-                val_loss += single_loss
+                batch_val = 0.0
+                for loss_name, loss in losses.items():
+                    batch_val += loss.mean().item()
+                val_loss_sum += batch_val
+                val_batch_count += 1
+            avg_val_loss = val_loss_sum / max(val_batch_count, 1)
 
-        if epoch % 50==0: 
-            print("Epoch %d, Train loss %0.6f, Val loss %0.6f, iteration time %0.6f" % (epoch, train_loss, val_loss, time.time() - start_time))
+        epoch_time = time.time() - start_time
+
+        # 每 50 个 epoch 打印一次当前 epoch 的 loss
+        if epoch % 50 == 0:
+            print("Epoch %d, Train loss %0.6f, Val loss %0.6f, time %0.4fs" % (epoch, avg_train_loss, avg_val_loss, epoch_time))
         model.train()
 
 
